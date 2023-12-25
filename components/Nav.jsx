da86d59e1e07aed14cfd0e574280fe0da2ddffc6
Thirana <t.embuldeniya@gmail.com>
2023-12-25 12:22:59 +0530

    Form UI Complete

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,10 +2,8 @@
 
 import Link from "next/link";
 import Image from "next/image";
-
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
-import User from "@models/user";
 
 const Nav = () => {
   const { data: session } = useSession();
@@ -14,13 +12,10 @@ const Nav = () => {
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setUpProviders = async () => {
-      const response = await getProviders();
-
-      setProviders(response);
-    };
-
-    setUpProviders();
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })();
   }, []);
 
   return (
@@ -36,7 +31,7 @@ const Nav = () => {
         <p className="logo_text">Promptopia</p>
       </Link>
 
-      {/* Desktop navigation */}
+      {/* Desktop Navigation */}
       <div className="sm:flex hidden">
         {session?.user ? (
           <div className="flex gap-3 md:gap-5">
@@ -50,7 +45,7 @@ const Nav = () => {
 
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -82,12 +77,12 @@ const Nav = () => {
         {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
               alt="profile"
-              onClick={() => setToggleDropdown((prev) => !prev)}
+              onClick={() => setToggleDropdown(!toggleDropdown)}
             />
 
             {toggleDropdown && (
@@ -106,7 +101,6 @@ const Nav = () => {
                 >
                   Create Prompt
                 </Link>
-
                 <button
                   type="button"
                   onClick={() => {
