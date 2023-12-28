4f38af8564fa6c979b6e2954b07e82c6eaab76ed
Thirana <t.embuldeniya@gmail.com>
2023-12-28 11:25:58 +0530

    Update and delete complete

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -10,7 +10,7 @@ const MyProfile = () => {
   const router = useRouter();
   const { data: session } = useSession();
 
-  const [myPosts, setMyPosts] = useState([]);
+  const [posts, setMyPosts] = useState([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -23,15 +23,37 @@ const MyProfile = () => {
     if (session?.user.id) fetchPosts();
   }, [session?.user.id]);
 
-  const handleEdit = () => {};
+  const handleEdit = (post) => {
+    // This will redirect user to edit page
+    router.push(`/update-prompt?id=${post._id}`);
+  };
 
-  const handleDelete = async () => {};
+  const handleDelete = async (post) => {
+    // This is inbuild browser method
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = posts.filter((item) => item._id !== post._id);
+
+        setMyPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <Profile
       name="My"
       desc="Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination"
-      data={myPosts}
+      data={posts}
       handleEdit={handleEdit}
       handleDelete={handleDelete}
     />
